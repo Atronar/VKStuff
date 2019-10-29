5394377eb68453b9d3255299c60c15b0fdef8563
Atronar <atronar@users.noreply.github.com>
2019-10-30 04:37:05 +0700

    Обновлён файл авторизации, теперь капча будет выводиться в отдельном окне

--- vkAuth.py
+++ vkAuth.py
@@ -1,9 +1,12 @@
-﻿import vk_api;
+﻿# v. 191030
+import vk_api;
 from getpass import getpass;
 import json;
 from os.path import exists as fileexists;
 import time;
 import vk_api.upload;
+import cv2;
+import requests;
 
 # Двухфакторная авторизация
 def auth_handler():
@@ -14,16 +17,21 @@ def auth_handler():
    return key, remember_device
 
 # Обработка капчи
-def captcha_handler(captcha, key=None):
+def captcha_handler(captcha, key=None, show_captcha=False):
    """ При возникновении капчи вызывается эта функция и ей передается объект
        капчи. Через метод get_url можно получить ссылку на изображение.
        Через метод try_again можно попытаться отправить запрос с кодом капчи
    """
-   if 'VkApi._vk_login' in str(captcha.func):
+   if 'VkApi._vk_login' in f"{captcha.func}":
       raise vk_api.exceptions.AuthError()
-   if key is None:
+   if not key:
       try:
-         key = input("Введите капчу {0}: ".format(captcha.get_url()))
+         captcha_url = captcha.get_url();
+         if show_captcha:
+            show_img(requests.get(captcha_url).content);
+         key = input(f"Введите капчу {captcha_url}: ");
+         if show_captcha:
+            cv2.destroyAllWindows();
       except EOFError:
          key = input()
    key = key.strip()
@@ -32,8 +40,8 @@ def captcha_handler(captcha, key=None):
 
 def captcha_saver(captcha):
    with open('captcha','wt',encoding='utf-8') as f:
-      for s in [captcha.sid, captcha.func, captcha.args, captcha.kwargs, captcha.url, time.time()]:
-         f.write(str(s)+'\n')
+      for s in (captcha.sid, captcha.func, captcha.args, captcha.kwargs, captcha.url, time.time()):
+         f.write(f'{s}\n')
    print(captcha);
    print('\a');
 
@@ -57,25 +65,22 @@ def vk_auth(login=None,password=None,captcha_handler=captcha_saver):
    if password is not None:
       vk = vk_api.VkApi(login, password, auth_handler=auth_handler, captcha_handler=captcha_handler, app_id=6479356, scope=140492255);
       if fileexists("vk_config.v2.json"):
-         vk_config_json = open("vk_config.v2.json");
-         vk_config_data = vk_config_json.read();
-         vk_access_token = json.loads(vk_config_data)[str(login)]['token']["app6479356"]["scope_140492255"]['access_token'];
-         vk_config_json.close();
+         with open("vk_config.v2.json") as vk_config_json:
+            vk_config_data = vk_config_json.read();
+            vk_access_token = json.loads(vk_config_data)[f"{login}"]['token']["app6479356"]["scope_140492255"]['access_token'];
       else:
          vk_access_token = vk.token['access_token'];
    else:
       if fileexists("vk_config.v2.json"):
-         vk_config_json = open("vk_config.v2.json");
-         vk_config_data = vk_config_json.read();
-         if str(login) in json.loads(vk_config_data):
-            vk_access_token = json.loads(vk_config_data)[str(login)]['token']["app6479356"]["scope_140492255"]['access_token'];
-            vk_config_json.close();
-            vk = vk_api.VkApi(login, auth_handler=auth_handler, captcha_handler=captcha_handler, app_id=6479356, scope=140492255);
-         else:
-            vk_config_json.close();
-            password = getpass("Введите пароль: ");
-            vk = vk_api.VkApi(login, password, auth_handler=auth_handler, captcha_handler=captcha_handler, app_id=6479356, scope=140492255);
-            vk_access_token = vk.token['access_token'];
+         with open("vk_config.v2.json") as vk_config_json:
+            vk_config_data = vk_config_json.read();
+            if f"{login}" in json.loads(vk_config_data):
+               vk_access_token = json.loads(vk_config_data)[f"{login}"]['token']["app6479356"]["scope_140492255"]['access_token'];
+               vk = vk_api.VkApi(login, auth_handler=auth_handler, captcha_handler=captcha_handler, app_id=6479356, scope=140492255);
+            else:
+               password = getpass("Введите пароль: ");
+               vk = vk_api.VkApi(login, password, auth_handler=auth_handler, captcha_handler=captcha_handler, app_id=6479356, scope=140492255);
+               vk_access_token = vk.token['access_token'];
       else:
          password = getpass("Введите пароль: ");
          vk = vk_api.VkApi(login, password, auth_handler=auth_handler, captcha_handler=captcha_handler, app_id=6479356, scope=140492255);
@@ -90,6 +95,42 @@ def vk_auth(login=None,password=None,captcha_handler=captcha_saver):
    except vk_api.exceptions.ApiError:
       vk, vk_access_token, my_id, my_screen_id = vk_auth(login,getpass("Введите пароль: "))
    return vk, vk_access_token, my_id, my_screen_id;
+
+# Взятие лучшего по качеству изображения
+# Принимает attach["photo"]
+# Возвращает словарь вида {'url', 'width', 'height'}
+def get_max_photo(photo):
+   # attach["photo"]['sizes'] возвращает словарь
+   if 'sizes' in photo:
+      for size_item in photo['sizes']:
+         try:
+            if size_item['width']==max(i['width'] for i in photo['sizes']):
+               return size_item
+         except:
+            print(photo['sizes'])
+            raise
+   # attach["photo"] возвращает ссылку
+   else:
+      # Берём фото лучшего качества
+      if "photo_2560" in photo:
+         photo_size = "photo_2560";
+      elif "photo_1280" in photo:
+         photo_size = "photo_1280";
+      elif "photo_807" in photo:
+         photo_size = "photo_807";
+      elif "photo_604" in photo:
+         photo_size = "photo_604";
+      elif "photo_130" in photo:
+         photo_size = "photo_130";
+      elif "photo_75" in photo:
+         photo_size = "photo_75";
+      return {'url': photo[photo_size], 'width':photo['width'], 'height':photo['height']}
+
+# Функция показа изображения без перехвата управления вызванным окном
+def show_img(img_file,title=''):
+   cv_img = cv2.imdecode(np.asarray(bytearray(img_file)), 1);
+   cv2.imshow(title,cv_img);
+   cv2.waitKey(1);
 
 # В классе некоторые исправления
 class betterVkUpload(vk_api.upload.VkUpload):
